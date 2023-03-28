86f4aae5277abe7f6ae9d4c7d0b3a86d8232c2d0
NikolaZim <n.zimenkov.2020@gmail.com>
2023-03-28 13:01:36 +0300

    devNew

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
 /*
 === Базовая задача ===
 
@@ -12,6 +20,38 @@ package main
 Программа должна проходить проверки go vet и golint.
 */
 
+var ntpServer = "0.beevik-ntp.pool.ntp.org"
+
 func main() {
+	//Обработка паники и возврат ненулевого кода выхода в OS
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Fprintln(os.Stderr, "Error:", r)
+			os.Exit(1)
+		}
+	}()
+	//Выход из программы
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT)
+	go func() {
+		<-c
+		fmt.Println("Got SIGINT signal")
+		// Дополнительный код обработки сигнала
+		os.Exit(0)
+	}()
+	//Получение времени от ntp сервера
+	flagT := "f"
+	for {
+		fmt.Println("Для получения точного времени введите t:")
+		if fmt.Scan(&flagT); flagT == "t" {
+			time, err := ntp.Time(ntpServer)
+			if err != nil {
+				panic(err)
+			} else {
+				fmt.Println(time)
+			}
+			flagT = "f"
+		}
+	}
 
 }
--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 
@@ -13,6 +22,54 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+// структура с параметрами
+type flags struct {
+	fields    int
+	delimiter string
+	separated bool
+}
 
+func main() {
+	// обработка флагов
+	myFlags := flags{}
+	flag.IntVar(&myFlags.fields, "f", 0, "выбрать поля (колонки)")
+	flag.StringVar(&myFlags.delimiter, "d", " ", "использовать другой разделитель")
+	flag.BoolVar(&myFlags.separated, "s", false, "только строки с разделителем")
+	flag.Parse()
+
+	filename := "test_text.txt"
+	args := flag.Args()
+	if len(args) > 1 {
+		log.Fatalln("qwer")
+	}
+	if len(args) == 1 {
+		filename = args[0]
+	}
+
+	input := ""
+	if filename != "" {
+		bytes, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = string(bytes)
+	} else {
+		bytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		input = string(bytes)
+	}
+
+	lines := strings.Split(input, "\n")
+
+	for _, line := range lines {
+		cols := strings.Split(line, f.D)
+
+		if f.F > 0 && f.F <= len(cols) {
+			fmt.Println(cols[f.F-1])
+		} else if !f.S {
+			fmt.Println(line)
+		}
+	}
 }
